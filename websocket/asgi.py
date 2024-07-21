0ccb77d3bb967dd768735523a38bd69d87e1816d
Landvoigt <tim.voigt@ik.me>
2024-07-21 14:05:05 +0200

    first commit

---
+++ websocket/asgi.py
@@ -0,0 +1,23 @@
+import os
+
+from django.core.asgi import get_asgi_application
+
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.routing import websocket_urlpatterns
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "websocket.settings")
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
